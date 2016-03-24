dd3ee41d7cd9cfdee3aaf343c2d47d4dd9ca8601
KhoaNguyen2209 <n_nhukhoa@yahoo.com.vn>
2016-03-24 21:53:58 +0700

    add configuration parameter

---
+++ SoundCloudPlayer/app/src/main/java/usth/edu/vn/soundcloudplayer/Config.java
@@ -0,0 +1,9 @@
+package usth.edu.vn.soundcloudplayer;
+
+/**
+ * Created by Khoa on 23-Mar-16.
+ */
+public class Config {
+    public static final String CLIENT_ID = "2b7b41a5993a0e6ff05de2c2c42a82e6";
+    public static final String API_URL = "https://api.soundcloud.com";
+}
