66ac69c5b967d64d0b0bd24eb55daa57921325cb
KhoaNguyen2209 <n_nhukhoa@yahoo.com.vn>
2016-03-24 21:57:30 +0700

    add Playbar to a new Fragment

---
+++ SoundCloudPlayer/app/src/main/java/fragment/PlaybarFragment.java
@@ -0,0 +1,30 @@
+package fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import usth.edu.vn.soundcloudplayer.R;
+
+/**
+ * Created by Khoa on 22-Mar-16.
+ */
+public class PlaybarFragment extends Fragment {
+    public PlaybarFragment(){}
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_playbar, null);
+        return v;
+    }
+}
